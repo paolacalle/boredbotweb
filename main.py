d2dec2e4e9def0009161ef2be4f892f54a7ffb7c
Paola Calle <paolac@seoscholars.org>
2023-07-07 06:18:31 +0000

    Changes

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def register():
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/boredbot/boredbot2')
+        repo = git.Repo('/home/boredbotweb/boredbotweb')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
